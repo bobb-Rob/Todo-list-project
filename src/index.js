f5cdbfe31965afe5d5b7b145c36a9be75747a0c3
Robertson Akpan <robertsonakpan@gmail.com>
2022-06-01 09:49:56 +0100

    update function to load on document content loaded

--- src/index.js
+++ src/index.js
@@ -1,44 +1,34 @@
-
+import todos from './todoItems';
 import './style.css';
 
 
-const todos = [
-  {
-    description: 'Creating a frontend project',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Baking cakes',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Reading about local storage',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Cleaning the house',
-    completed: false,
-    index: 4,
-  },
-];
 
-todos.forEach((todo) => {
+
+const todoItem = ({description, index}) => {
   const liEl = document.createElement('li');
   liEl.classList.add('todo-wrapper');
+  liEl.id = `todo-${index}`
   liEl.innerHTML = `
+    <div class='left-align'>
       <div>
-        <input type="checkbox" id="${todo.index}" />
+        <input type="checkbox" id="${index}" />
       </div>
       <div>
-        <p contenteditable="true">${todo.description}</p>
+        <p contenteditable="true">${description}</p>
       </div>
+    </div>
       <div>
         <ion-icon name="ellipsis-vertical-outline"></ion-icon>
     </div>
       `;
-      const listContainer = document.querySelector('.todo-list-holder');
-      listContainer.appendChild(liEl);
-});
+    return liEl;
+}
+
+const displayTodo = () => {
+  const listContainer = document.querySelector('.todo-list-holder');
+  todos.forEach((todo) => {
+  listContainer.appendChild(todoItem(todo));
+  });
+}
+
+document.addEventListener('DOMContentLoaded', displayTodo)
---
+++ src/todoItems.js
@@ -0,0 +1,25 @@
+
+const todos = [
+    {
+      description: 'Creating a handheld batton',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Baking cakes',
+      completed: false,
+      index: 2,
+    },
+    {
+      description: 'Reading about local storage',
+      completed: false,
+      index: 3,
+    },
+    {
+      description: 'Cleaning the house',
+      completed: false,
+      index: 4,
+    },
+  ];
+
+  export default todos;
